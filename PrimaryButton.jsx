c8b61e3931a19f94954e840e359e932165384324
unknown <phonepadithpp@gmail.com>
2026-04-26 11:08:58 +0700

    update new version

---
+++ PrimaryButton.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Primarybutton = () => {
+  return (
+  <button aria-label="Submit form" role="button" style={{"backgroundColor":"#007bff","color":"#ffffff","padding":"10px 20px","border":"none","borderRadius":"4px","fontSize":"16px","cursor":"pointer"}}>Click Me</button>
+  );
+};
+
+export default Primarybutton;
